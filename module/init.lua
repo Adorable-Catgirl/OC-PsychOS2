1ba6cd3ade816e88039e6ba12b4e0e293b333068
XeonSquared <redtailedairways@gmail.com>
2019-01-08 17:59:01 +1100

    made init cope if there is no init.txt

--- module/init.lua
+++ module/init.lua
@@ -1,15 +1,18 @@
 os.spawn(function() print(pcall(function()
 print(_OSVERSION,tostring(math.floor(computer.totalMemory()/1024)).."K memory")
+os.setenv("PWD","/boot")
 local f = fs.open("/boot/init.txt","rb")
-local fc = f:read("*a")
-f:close()
-for line in fc:gmatch("[^\n]+") do
- print("Starting service "..line)
- spawnfile("/boot/service/"..line)
+if f then
+ local fc = f:read("*a")
+ f:close()
+ for line in fc:gmatch("[^\n]+") do
+  print("Starting service "..line)
+  spawnfile("/boot/service/"..line,line)
+ end
 end
 for k,v in pairs(fd) do
  if v.t == "t" then
-  tTasks[cPid].t = k
+  os.setenv("t",k)
   print("Spawning a shell for terminal #"..tostring(k))
   spawnfile("/boot/exec/shell.lua","shell #"..tostring(k))
  end
