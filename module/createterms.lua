9ee9b5d6a2a4aec3cd9cc7a160238d1c5b01fc32
XeonSquared <redtailedairways@gmail.com>
2019-01-08 18:08:25 +1100

    added a module only to create fds from GPU+Screen+Keyboard combos

---
+++ module/createterms.lua
@@ -0,0 +1,8 @@
+local ts = {}
+for a,_ in component.list("screen") do
+ ts[#ts+1] = a
+end
+for a,_ in component.list("gpu") do
+ local r,w = vtemu(a,table.remove(ts,1))
+ fd[#fd+1] = {["read"]=r,["write"]=w,["close"]=function() w("\27[2J\27[H") end,["t"]="t"}
+end
