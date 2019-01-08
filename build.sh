b28991241cd77a9100de484f6e7dca7377f1101d
XeonSquared <redtailedairways@gmail.com>
2019-01-08 18:11:37 +1100

    updated build.sh to reflect the current state of the OS it will also copy ../OC-Minitel/minitel.lua into ./service/minitel.lua if possible, for current development work

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+cp ../OC-Minitel/minitel.lua service/minitel.lua
 mkdir build
 cd module
-cat sched.lua vt100.lua fs.lua loadfile.lua vt-task.lua io.lua microtel-3.lua init.lua > ../build/psychos.lua
+cat sched.lua vt100.lua fs.lua loadfile.lua vt-task.lua io.lua createterms.lua init.lua > ../build/psychos.lua
 cd ..
 echo '_OSVERSION="PsychOS 2.0a0"' >> build/*
 echo sched\(\) >> build/*
