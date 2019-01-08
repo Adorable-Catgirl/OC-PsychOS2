0928241895cce5adf6bbb0840206923f20d065dc
XeonSquared <redtailedairways@gmail.com>
2019-01-08 18:15:22 +1100

    removed the whole mod-service thing

--- mkmods.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for f in service/*; do
- nf=$(echo $f | cut -d '/' -f 2)
- echo $nf
- echo os.spawn\(function\(\) >mod-service/$nf
- cat $f >> mod-service/$nf
- echo end,\"$nf\"\) >> mod-service/$nf
-done
