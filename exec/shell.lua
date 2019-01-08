8b35bfe3a6c35e57e293febb91e7952f70079913
XeonSquared <redtailedairways@gmail.com>
2019-01-08 18:03:51 +1100

    made the shell cleaner, and tell the current $PWD

--- exec/shell.lua
+++ exec/shell.lua
@@ -1,8 +1,10 @@
 local shenv = {}
 setmetatable(shenv,{__index=function(_,k) if _G[k] then return _G[k] elseif fs.exists("/boot/exec/"..k..".lua") then return loadfile("/boot/exec/"..k..".lua") end end})
+print(_VERSION)
 while true do
- io.write(_VERSION.."> ")
+ io.write((os.getenv("PWD") or _VERSION).."> ")
  tResult = {pcall(load(io.read(),"shell","t",shenv))}
+ if tResult[1] == true then table.remove(tResult,1) end
  for k,v in pairs(tResult) do
   print(v)
  end
