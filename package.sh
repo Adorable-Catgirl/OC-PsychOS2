77c28adefa747f3bf64780072b78326bedab6667
XeonSquared <redtailedairways@gmail.com>
2019-07-18 13:41:15 +1000

    added a tarbomb version of the cpio archive, for using with tapeloader

--- package.sh
+++ package.sh
@@ -3,4 +3,7 @@ rm -r psychos
 mkdir psychos
 cp -r exec/ lib/ service/ psychos/
 cp build/psychos.lua psychos/init.lua
+cp default-init.txt psychos/init.txt
 tree -if psychos/ | cpio -oHbin > psychos.cpio
+cd psychos
+tree -if | cpio -oHbin > ../psychos-tarbomb.cpio
