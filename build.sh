2aca6df1b4f6fe962d5aceff48214ba90dc317a5
XeonSquared <redtailedairways@gmail.com>
2019-07-22 13:54:39 +1000

    cleaned up some meta stuff

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-cp ../OC-Minitel/minitel.lua service/minitel.lua
 mkdir build
 cd module
 cat sched.lua syslog.lua vt100.lua fs.lua iofs.lua loadfile.lua vt-task.lua io.lua createterms.lua init.lua > ../build/psychos.lua
--- package.sh
+++ package.sh
@@ -4,6 +4,6 @@ mkdir psychos
 cp -r exec/ lib/ service/ psychos/
 cp build/psychos.lua psychos/init.lua
 cp default-init.txt psychos/init.txt
-tree -if psychos/ | cpio -oHbin > psychos.cpio
+find psychos/ | cpio -oHbin > psychos.cpio
 cd psychos
-tree -if | cpio -oHbin > ../psychos-tarbomb.cpio
+find | cpio -oHbin > ../psychos-tarbomb.cpio
