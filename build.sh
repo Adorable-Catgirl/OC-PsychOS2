a428a36c5d631d8477728905bb990193f7ba7efb
XeonSquared <redtailedairways@gmail.com>
2019-11-06 14:01:10 +1100

    made build.sh add an _OSVERSION string

--- build.sh
+++ build.sh
@@ -2,4 +2,7 @@
 rm -r target/*
 mkdir target
 lua luapreproc.lua module/init.lua target/init.lua
+echo _OSVERSION=\"PsychOS 2.0a1-$(git rev-parse --short HEAD)\" > target/version.lua
+cat target/version.lua target/init.lua > target/tinit.lua
+mv target/tinit.lua target/init.lua
 cp -r exec/ service/ lib/ target/
