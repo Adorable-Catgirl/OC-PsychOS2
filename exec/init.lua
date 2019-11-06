1c416be625cab6365c904e5229c95e8021a29690
XeonSquared <redtailedairways@gmail.com>
2019-11-06 14:31:28 +1100

    made the version thingo print a line this is important

--- exec/init.lua
+++ exec/init.lua
@@ -6,7 +6,7 @@ for k,v in pairs(fs.list("/dev/")) do
  if v:sub(1,3) == "tty" then
   dprint(tostring(io.input("/dev/"..v)))
   dprint(tostring(io.output("/dev/"..v)))
-  io.write(_OSVERSION.." - "..tostring(math.floor(computer.totalMemory()/1024)).."K RAM")
+  print(_OSVERSION.." - "..tostring(math.floor(computer.totalMemory()/1024)).."K RAM")
   os.spawnfile("/boot/exec/shell.lua")
  end
 end
