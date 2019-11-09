d23a25613dae8412f559e1de9097b0b5e925b0c3
XeonSquared <redtailedairways@gmail.com>
2019-11-09 13:48:50 +1100

    made ps display the pid correctly

--- exec/ps.lua
+++ exec/ps.lua
@@ -1,5 +1,5 @@
 print("PID# Parent | Name")
 for k,v in pairs(os.tasks()) do
  local t = os.taskInfo(v)
- print(string.format("%4d   %4d | %s",k,t.parent,t.name))
+ print(string.format("%4d   %4d | %s",v,t.parent,t.name))
 end
