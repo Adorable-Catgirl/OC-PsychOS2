b66e8f93a751572126de03ebc8d04bc502110bc7
XeonSquared <redtailedairways@gmail.com>
2019-11-19 17:19:02 +1100

    made free floor all numbers

--- exec/free.lua
+++ exec/free.lua
@@ -1,2 +1,2 @@
 print("Total  Used  Free")
-print(string.format("%4dK %4dK %4dK",computer.totalMemory()/1024,math.floor((computer.totalMemory()-computer.freeMemory())/1024),math.floor(computer.freeMemory()/1024)))
+print(string.format("%4iK %4iK %4iK",math.floor(computer.totalMemory()/1024),math.floor((computer.totalMemory()-computer.freeMemory())/1024),math.floor(computer.freeMemory()/1024)))
