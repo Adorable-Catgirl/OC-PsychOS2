6dfb34bb724e0c6b7ba0239fbc528089bd29abd6
XeonSquared <redtailedairways@gmail.com>
2019-11-20 08:30:38 +1100

    added minitel to the default init, updated the package script

--- package.sh
+++ package.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-rm -r psychos
-mkdir psychos
-cp -r exec/ lib/ service/ psychos/
-cp build/psychos.lua psychos/init.lua
-cp default-init.txt psychos/init.txt
-find psychos/ | cpio -oHbin > psychos.cpio
-cd psychos
+#rm -r psychos
+#mkdir psychos
+#cp -r exec/ lib/ service/ psychos/
+#cp build/psychos.lua psychos/init.lua
+#cp default-init.txt psychos/init.txt
+#find psychos/ | cpio -oHbin > psychos.cpio
+cd target
 find | cpio -oHbin > ../psychos-tarbomb.cpio
